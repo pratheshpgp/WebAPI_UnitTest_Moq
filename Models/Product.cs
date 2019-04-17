5b9c095cbe8e0b691967afde0e170c05a722d2ed
pratheshpgp <45036226+pratheshpgp@users.noreply.github.com>
2019-04-17 12:02:53 +0530

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnitTest.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        public string productName { get; set; }
+    }
+}
